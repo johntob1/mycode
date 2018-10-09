1f2d9da798d00ac0f580c69ff725a57db2b16d22
Toby John <toby.john@verizonwireless.com>
2018-10-09 09:32:49 -0400

    your commit message

---
+++ lab_input/input_ip.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+user_input = input('Please enter an IPv4 IP address:')
+print("You told me the IPv4 address is:" + user_input)
+
+user_input = input('vendor name:')
+print("You told me the vendor name is:" + user_input)
